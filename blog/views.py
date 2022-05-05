b4fb69efdeeb739de3edb90cce85e51cd924ed8c
Your Name <you@example.com>
2022-05-05 14:17:10 +0800

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/views.py
+++ blog/views.py
@@ -9,14 +9,7 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
     Post.objects.get(pk=pk)
-    
-    
-    
-def post_list(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_list.html', {'post': post})
 
-    Post.objects.get(pk=pk)
-    
-
-    
+def post_list(request):
+     post = Post.objects.all()
+     return(request, "blog/post_list.html", {"post": post})
