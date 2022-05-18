10676cb20f8e0dd0a96220a600eda3cb3b8b4ed0
Your Name <you@example.com>
2022-05-18 23:26:19 +0800

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 
 from django.utils import timezone
-from blog.templates.blog.forms import PostForm
+from .forms import PostForm
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -14,7 +15,7 @@ def post_detail(request, pk):
 
     Post.objects.get(pk=pk)
     
-    
+
       
 def post_list(request):
      post = Post.objects.all()
@@ -26,7 +27,6 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
@@ -40,14 +40,23 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
 
-
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
